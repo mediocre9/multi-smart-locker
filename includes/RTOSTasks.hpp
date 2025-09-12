bb438b9a13ce1eb9bc695de3d828364d650b595f
mediocre9 <mirzafahadzia9@gmail.com>
2025-09-13 02:11:05 +0500

    feat: add RTOSTasks for OTA updates and Firebase listener functionality

---
+++ includes/RTOSTasks.hpp
@@ -0,0 +1,90 @@
+#ifndef RTOS_TASK_H
+#define RTOS_TASK_H
+
+#include <ArduinoJson.h>
+#include <VoyagerOTA.hpp>
+#include "../includes/Config.hpp"
+#include "../includes/HttpServer.hpp"
+#include "../includes/NetworkOperationManager.hpp"
+#include "../includes/WebSocket.hpp"
+
+namespace RTOSTask {
+    void otaUpdateHandlerTask(void* _) {
+        Voyager::OTA<> ota(Device::FIRMWARE_VERSION);
+        ota.setEnvironment(Voyager::Environment::PRODUCTION);
+        ota.setGlobalHeaders({
+            {"x-api-key", AuthKeys::VOYAGER_PROJECT_API_KEY},
+            {"x-project-id", AuthKeys::VOYAGER_PROJECT_ID},
+        });
+
+        if (!NetworkOperationManager::hasFoundInternet()) {
+            LOGLN(ResponseMessage::NO_INTERNET_CONNECTION);
+            return;
+        }
+
+        std::optional<Voyager::VoyagerReleaseModel> release = ota.fetchLatestRelease();
+        if (!release) {
+            LOGLN("No updates available!");
+            return;
+        }
+
+        if (release && ota.isNewVersion(release->version)) {
+            ota.performUpdate();
+        } else if (release->statusCode == HTTP_CODE_NOT_FOUND) {
+            LOGLN(release->message);
+        } else {
+            LOGLN(release->message);
+        }
+    }
+
+    void firebaseListenerTask(void* _) {
+        unsigned long previousMillisClient = 0;
+        unsigned long previousMillisFirebase = 0;
+        const unsigned long CLIENT_INTERVAL = 1000;  // 1 sec . . .
+
+#if PROD_MODE
+        const unsigned long FIREBASE_INTERVAL = 600 * 1000;  // 600 seconds (10 minutes) . . .
+#else
+        const unsigned long FIREBASE_INTERVAL = 60 * 1000;  // 60 seconds (1 minute) . . .
+#endif
+
+        for (;;) {
+            unsigned long currentMillis = millis();
+            if ((currentMillis - previousMillisFirebase) >= FIREBASE_INTERVAL) {
+                previousMillisFirebase = currentMillis;
+
+                if (WiFi.status() == WL_CONNECTED) {
+                    if (HttpServer::firebase.isAuthenticated()) {
+                        HttpServer::firebase.listenForAuthorizationStatus();
+                    }
+                }
+            }
+
+#if EXPERIMENTAL_FEATURE
+            // WebSocket client operations....
+            if ((currentMillis - previousMillisClient) >= CLIENT_INTERVAL) {
+                previousMillisClient = currentMillis;
+
+                ClientManager& manager = webSocket.getClients();
+                for (auto& [clientId, clientInfo] : manager) {
+                    if (clientInfo.timeout > 0) {
+                        clientInfo.timeout -= CLIENT_INTERVAL;
+                        webSocket.getWebSocketInstance().text(clientId, String(clientInfo.timeout));
+                        LOGLN("Client ID: " + String(clientId) + " " + clientInfo.email);
+                    }
+
+                    // disconnect client and close locker...
+                    if (clientInfo.timeout <= 0) {
+                        digitalWrite(clientInfo.gpio, HIGH);
+                        webSocket.getWebSocketInstance().close(clientId);
+                    }
+                }
+            }
+#endif
+
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
+    }
+}  // namespace RTOSTask
+
+#endif
